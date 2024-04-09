d4e84a41996b1da664db678db0d4da40675e6526
s137 <s137@esp2pc7.es.tu-harburg.de>
2024-04-09 11:02:53 +0200

    update

--- hello_world.cpp
+++ hello_world.cpp
@@ -23,53 +23,112 @@ struct TouchState {
     }
 };
 
+struct Robot {
+
+    Robot()
+        : m_left_wheel(motor(OUTPUT_A, motor::motor_large))
+        , m_right_wheel(motor(OUTPUT_B, motor::motor_large))
+        , m_head(motor(OUTPUT_D, motor::motor_medium))
+        , m_eye(ultrasonic_sensor(INPUT_4))
+        , m_touch(touch_sensor(INPUT_1)) {
+
+        const uint32_t speed = 30;
+        m_left_wheel.set_duty_cycle_sp(speed);
+        m_right_wheel.set_duty_cycle_sp(speed);
+
+        m_head_should_change_direction = false;
+        m_head_movement_speed = 100;
+        m_head_movement_direction = 1;
+
+        m_head.set_position(0);
+        m_head.set_duty_cycle_sp(m_head_movement_speed);
+
+        m_touch_state.init(&m_touch);
+    }
+
+    auto set_drive_speed(uint32_t speed) -> void {
+        m_left_wheel.set_duty_cycle_sp(speed);
+        m_right_wheel.set_duty_cycle_sp(speed);
+    }
+
+    auto drive() -> void {
+        m_left_wheel.run_direct();
+        m_right_wheel.run_direct();
+    }
+
+    auto stop_drive() -> void {
+        m_left_wheel.stop();
+        m_right_wheel.stop();
+    }
+
+    auto shutdown() -> void {
+        this->stop_drive();
+        m_head.stop();
+    }
+
+    motor             m_left_wheel;
+    motor             m_right_wheel;
+    motor             m_head;
+    ultrasonic_sensor m_eye;
+    touch_sensor      m_touch;
+
+    TouchState m_touch_state;
+
+    bool m_head_should_change_direction;
+    int  m_head_movement_speed;
+    int  m_head_movement_direction; // -1 left, 1 right
+};
+
 int main() {
     std::cout << "Hello CPS Course." << std::endl;
     std::cout << "Press main button to leave ..." << std::endl;
     // Wait for main butclearton is pressed
 
-    const uint32_t speed = 30;
-
-    bool  should_shut_down = false;
-    motor left_wheel = motor(OUTPUT_A, motor::motor_large);
-    motor right_wheel = motor(OUTPUT_B, motor::motor_large);
-
-    left_wheel.set_duty_cycle_sp(speed);
-    right_wheel.set_duty_cycle_sp(speed);
-
-    motor head = motor(OUTPUT_D, motor::motor_medium);
-
-    ultrasonic_sensor eye = ultrasonic_sensor(INPUT_4);
-
-    touch_sensor touch = touch_sensor(INPUT_1);
-    TouchState   touch_state = {};
-    touch_state.init(&touch);
+    Robot robot;
 
     do {
 
-        auto centimeter = eye.distance_centimeters();
-        auto angle = head.position();
+        auto centimeter = robot.m_eye.distance_centimeters();
+        auto angle = robot.m_head.position();
         // print out
         std::cout << "Distance: " << centimeter << " cm, Angle: " << angle << std::endl;
 
-        touch_state.update();
+        robot.m_touch_state.update();
 
-        if (touch_state.is_on && eye.distance_centimeters() > 20) {
-
-            touch_state.is_pressed_curr = touch.is_pressed();
-            if (eye.distance_centimeters() < 150) {
-                left_wheel.run_direct();
-                right_wheel.run_direct();
-            }
+        if (centimeter > 100) {
+            robot.set_drive_speed(100);
         } else {
-            left_wheel.stop();
-            right_wheel.stop();
-            touch_state.is_pressed_curr = touch.is_pressed();
+            robot.set_drive_speed(centimeter);
+        }
+
+        robot.m_head.run_direct();
+
+        const auto head_angle = 170;
+        if (abs(angle) > head_angle && !robot.m_head_should_change_direction) {
+            robot.m_head_should_change_direction = true;
+
+            robot.m_head_movement_direction = -robot.m_head_movement_direction;
+            auto sign = robot.m_head_movement_direction;
+            auto head_speed = robot.m_head_movement_speed;
+            robot.m_head.set_duty_cycle_sp(sign * head_speed);
+        }
+        if (abs(angle) <= head_angle) { robot.m_head_should_change_direction = false; }
+
+
+        if (angle) {
+
+            if (robot.m_touch_state.is_on && robot.m_eye.distance_centimeters() > 20) {
+
+                robot.m_touch_state.is_pressed_curr = robot.m_touch.is_pressed();
+                if (robot.m_eye.distance_centimeters() < 150) { robot.drive(); }
+            } else {
+                robot.stop_drive();
+                robot.m_touch_state.is_pressed_curr = robot.m_touch.is_pressed();
+            }
         }
 
     } while (!button::enter.pressed());
 
-    left_wheel.stop();
-    right_wheel.stop();
+    robot.shutdown();
     return 0;
 }
